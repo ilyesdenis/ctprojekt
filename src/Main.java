f03eeb3f411ca9a9838500b3b84328581a258cad
ilyesdenis <73537673+ilyesdenis@users.noreply.github.com>
2020-10-27 17:28:25 +0100

    *loadingscreen added (controller: Grafik)
    Signed-off-by: ilyesdenis <ilyesdenis@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
+public class Main extends Application{
+
+    public static void main(String[] args){
 	Grafik grafik=new Grafik();
 	grafik.startLoadingscreen();// asd
+        launch(args);
 
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
 
     }
 }
