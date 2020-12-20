3ad5144318a626f3e5b040b3938d33eee9d3b52e
ilyesdenis <ilyesdenis@gmail.com>
2020-12-20 16:27:12 +0100

    *loadingscreen added (controller: Grafik)
    Signed-off-by: ilyesdenis <ilyesdenis@gmail.com>

--- src/Grafik.java
+++ src/Grafik.java
@@ -2,6 +2,6 @@ public class Grafik {
     public void showKugelLandOn(int kugelpos) { //TODO a video should show kugel landing on kugelpos
     }
 
-    public void startLoadingscreen() { //TODO loadingscreen
+    public void startLoadingscreen() { //TODO loadingscreen(verbindung mit loadscreen.fxml)
     }
 }
