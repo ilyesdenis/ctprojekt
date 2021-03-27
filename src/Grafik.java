eaa084772a5a24bc668abde679f99bab138dc158
ilyesdenis <ilyesdenis@gmail.com>
2021-03-27 22:28:14 +0100

    changes made to Grafik and loadingscreen.fxml *the progressbar got the fx:id "pb" (in loadingscreen.fxml) *Resources folder added(mainly for .fxml files) *loadingsreen.fxml moved to Resources *some ongoing experiments added to Grafik(trying to get the progress bar functional, no success yet)
    Signed-off-by: ilyesdenis <ilyesdenis@gmail.com>

--- src/Grafik.java
+++ src/Grafik.java
@@ -1,19 +1,34 @@
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.stage.Stage;
+
 
 public class Grafik extends Application {
     public void showKugelLandOn(int kugelpos) { //TODO a video should show kugel landing on kugelpos
     }
 
+    @FXML
+    public ProgressBar pb;
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent loadingScreenRoot = FXMLLoader.load(getClass().getResource("loadingscreen.fxml"));
+        Parent loadingScreenRoot = FXMLLoader.load(getClass().getResource("Resources/loadingscreen.fxml"));
         primaryStage.setTitle("Roulette");
         primaryStage.setScene(new Scene(loadingScreenRoot));
         primaryStage.show();
+        // pb.setProgress(0.1); //TODO MAKE IT WORK!!!!!!!!!!!!!!!!!!!!
+
+
     }
+
+
+
+
+
 }
