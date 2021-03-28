6c5c4499d2a7cdc26792a759e9db0bcfba2844d7
ilyesdenis <ilyesdenis@gmail.com>
2021-03-28 22:14:41 +0200

    changes made to Grafik, loadingscreen.fxml and Main *on start the Main starts the first Stage *some cleanup done and some comments added *the progressBar is now kinda functional(if you click anywhere on the start screen, it does a fake loading) *a new TODO is added in Grafik *spent like 5 hours with this piece of shit
    Signed-off-by: ilyesdenis <ilyesdenis@gmail.com>

--- src/Grafik.java
+++ src/Grafik.java
@@ -4,28 +4,50 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import jdk.jfr.Experimental;
 
 
-public class Grafik extends Application {
+public class Grafik {
     public void showKugelLandOn(int kugelpos) { //TODO a video should show kugel landing on kugelpos
     }
+    boolean clickedtoStart=false;
+    @FXML public ProgressBar pb;
+   // @FXML public AnchorPane ap; !Experimental!
 
+    /* @FXML  Experimental!!Experimental!!Experimental!!Experimental!!Experimental!!Experimental!!Experimental!
+     public void onKeyPressed(KeyEvent keyEvent) {
+     if (keyEvent.getCode() == KeyCode.SPACE) {
+         System.out.println("Enter Pressed"); //Why the fuck dont´t you work
+         this.pb.setProgress(0.1);// ?
+     }
+ }*/
     @FXML
-    public ProgressBar pb;
-
-
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent loadingScreenRoot = FXMLLoader.load(getClass().getResource("Resources/loadingscreen.fxml"));
-        primaryStage.setTitle("Roulette");
-        primaryStage.setScene(new Scene(loadingScreenRoot));
-        primaryStage.show();
-        // pb.setProgress(0.1); //TODO MAKE IT WORK!!!!!!!!!!!!!!!!!!!!
-
+    public void onMauseClicked(MouseEvent mouseEvent) throws InterruptedException {
+        /*if (clickedtoStart=false) { TODO figure out WTF is wrong with this
+          */double i = 0;
+            do {
+                i = i + 0.0001;
+                pb.setProgress(i);
+                for (int j = 0; j < 1000000; j++) {
+                    for (int k = 0; k < 1000000; k++) {
+                    }
+                }
+                ;
+            } while (i < 1);
+        /* }
+       clickedtoStart=true; */
 
     }
+
+   /* @FXML !Experimental!!Experimental!!Experimental!!Experimental!!Experimental!!Experimental!!Experimental!
+    public  void initialise(){
+        pb.setProgress(0.1);
+    }*/
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) throws Exception {
 	Grafik grafik=new Grafik();
-    Application.launch(Grafik.class,args);
-
+       // grafik.initialise(); Experimental!!Experimental!!Experimental!!Experimental!!Experimental!!Experimental!!Experimental!
+    Application.launch();
     }
-
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/Resources/loadingscreen.fxml"));
+        primaryStage.setTitle("Roulette v0.0.01");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 
 }
