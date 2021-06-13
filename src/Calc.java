8ff43974bed4b16c8810ae8e76177d627fe801c8
ilyesdenis <ilyesdenis@gmail.com>
2021-06-13 20:40:27 +0200

    Logic updtate *Calc updated *Roulettfeld updated *bets added *payout calculator added *now spin() is in Calc
    Signed-off-by: ilyesdenis <ilyesdenis@gmail.com>

--- src/Calc.java
+++ src/Calc.java
@@ -1,2 +1,107 @@
+import java.util.Random;
+/*  bet IDs:
+* 0-36-the numbersx, 37-firsttwelvex, 38-onetoeighteenx, 39-evenx, 40-secoundtwelvex,41-thirdtwelvex
+* 42-redx, 43-blackx, 44-oddx, 45-nineteentothirtysixx, 47-row2x, 48-row3x, 49-row1x
+*
+*
+* */
+
 public class Calc {
+
+    private int[] bets;
+    Random rand = new Random();
+    private int kugelpos;
+
+    public int spinit() {
+        kugelpos = rand.nextInt(37);
+        return kugelpos;
+    }
+
+    public int calculateWin(int[] bets) {
+        this.bets = bets;
+        int win = 0;
+        for (int i = 0; i < 50; i++) {
+          //  int kugelpos = spinit();
+            if (bets[i]>0) { // bet on i is set
+                if (i < 37) {
+                    //*36
+                    if (kugelpos == i) {
+                        win = win + 36 * bets[i];//direct win, factor x36
+                        System.out.println("direct win on"+i);
+                    }
+                }
+                //*2
+                if (i == 39) { //39 even
+                    //System.out.println("DEBUG 39 EVEN");
+                    if (kugelpos % 2 == 0) {
+                       // System.out.println("DEBUG 39 ISEVEN");
+                        win = win + 2 * bets[39];// even win, factor x2
+                    }
+                }
+                if (i == 44) { //44 odd
+                    if (kugelpos % 2 == 1) {
+                        win = win + 2 * bets[44];// odd win, factor x2
+                    }
+                }
+                if (i == 38) { //38 onetoeighteen
+                    if ((kugelpos > 0) && (kugelpos < 19)) {
+                        win = win + 2 * bets[38];// 1-18 win, factor x2
+                    }
+                }
+                if (i == 45) { //45 nineteentothirtysix
+                    if ((kugelpos > 18) && (kugelpos < 37)) {
+                        win = win + 2 * bets[45];// 1-18 win, factor x2
+                    }
+                }
+                if (i == 42) { //42 red ,checks for 43 black too
+                    if ((kugelpos == 1) || (kugelpos == 3) || (kugelpos == 5) || (kugelpos == 7) || (kugelpos == 9) || (kugelpos == 12) || (kugelpos == 14) || (kugelpos == 16) || (kugelpos == 18) || (kugelpos == 19) || (kugelpos == 21) || (kugelpos == 23) || (kugelpos == 25) || (kugelpos == 27) || (kugelpos == 30) || (kugelpos == 32) || (kugelpos == 34) || (kugelpos == 36)) {
+                        win = win + 2 * bets[42];// red win, factor x2
+                    }/*else if ((bets[43] > 0) && (kugelpos>0)) { //////////DOES  NOT WORK!!!!!!!!!!!!!
+                        win = win + 2 * bets[43];// black win, factor x2
+                    }*/
+                }
+                if(i==43){
+                    if ((kugelpos == 2) || (kugelpos == 4) || (kugelpos == 6) || (kugelpos == 8) || (kugelpos == 10) || (kugelpos == 11) || (kugelpos == 13) || (kugelpos == 15) || (kugelpos == 17) || (kugelpos == 20) || (kugelpos == 22) || (kugelpos == 24) || (kugelpos == 26) || (kugelpos == 28) || (kugelpos == 29) || (kugelpos == 31) || (kugelpos == 33)|| (kugelpos == 35)) {
+                        win = win + 2 * bets[43];// black win, factor x2
+                    }
+                }
+
+
+                //x3
+                if (i == 37) { //37 first12
+                    if ((kugelpos > 0) && (kugelpos < 13)) {
+                        win = win + 3 * bets[37]; //first 12 win, factor x3
+                    }
+                }
+                if (i == 40) { //40 secoundtwelve
+                    if ((kugelpos > 12) && (kugelpos < 25)) {
+                        win = win + 3 * bets[40]; //secound 12 win, factor x3
+                    }
+                }
+                if (i == 41) { //41 thirdtwelve
+                    if ((kugelpos > 24) && (kugelpos < 37)) {
+                        win = win + 3 * bets[41]; //third 12 win, factor x3
+                    }
+                }
+                if (i == 49) { //49 row1
+                    if (kugelpos % 3 == 1) {
+                        win = win + 3 * bets[49]; //row1 win, factor x3
+                    }
+                }
+                if (i == 47) { //47 row2
+                    if (kugelpos % 3 == 2) {
+                        win = win + 3 * bets[47]; //row2 win, factor x3
+                    }
+                }
+                if (i == 48) { //49 row1
+                    if (kugelpos % 3 == 0) {
+                        win = win + 3 * bets[48]; //row3 win, factor x3
+                    }
+                }
+            }
+        }
+        System.out.println("win:"+win);
+        return win;
+
+    }
 }
--- src/RoulettefeldController.java
+++ src/RoulettefeldController.java
@@ -7,17 +7,17 @@ import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
-import java.awt.*;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.Random;
+//import java.util.Random;
 
 
 public class RoulettefeldController implements Initializable {
 
-    Random rand = new Random();
+  //  Random rand = new Random();
 
     @FXML
 
@@ -34,57 +34,9 @@ public class RoulettefeldController implements Initializable {
     int kugelpos, spin = 0;
 
 
-    private int betRed = 0;
-    private int betBlack = 0;
-    private int betEven = 0;
-    private int betOdd = 0;
-    private int betFirst12 = 0;
-    private int betSecond12 = 0;
-    private int betLast12 = 0;
-    private int betRow1 = 0;
-    private int betRow2 = 0;
-    private int betRow3 = 0;
-    private int betOneToEighteen = 0;
-    private int betNineteenToThirtysix = 0;
-
-
-    private int betZero = 0;
-    private int betOne = 0;
-    private int betTwo = 0;
-    private int betThree = 0;
-    private int betFour = 0;
-    private int betFive = 0;
-    private int betSix = 0;
-    private int betSeven = 0;
-    private int betEight = 0;
-    private int betNine = 0;
-    private int betTen = 0;
-    private int betEleven = 0;
-    private int betTwelve = 0;
-    private int betThriteen = 0;
-    private int betFourteen = 0;
-    private int betFifteen = 0;
-    private int betSixteen = 0;
-    private int betSeventeen = 0;
-    private int betEighteen = 0;
-    private int betNineteen = 0;
-    private int betTwenty = 0;
-    private int betTwentyone = 0;
-    private int betTwentytwo = 0;
-    private int betTwentythree = 0;
-    private int betTwentyfour = 0;
-    private int betTwentyfive = 0;
-    private int betTwentysix = 0;
-    private int betTwentyseven = 0;
-    private int betTwentyeight = 0;
-    private int betTwentynine = 0;
-    private int betThirty = 0;
-    private int betThirtyone = 0;
-    private int betThirtytwo = 0;
-    private int betThirtythree = 0;
-    private int betThirtfour = 0;
-    private int betThirtyfive = 0;
-    private int betThirtysix = 0;
+    private int[] bets=new int[50];
+    int chips=10;
+    private Calc calc=new Calc();
 
 
 // winning Number displayed
@@ -92,10 +44,13 @@ public class RoulettefeldController implements Initializable {
     @FXML
     void DisplayNumber(ActionEvent event) throws InterruptedException {
         spin();
+        this.kugelpos=getspin();
         spin++;
-        kugelpos = rand.nextInt(37);
+       // kugelpos = rand.nextInt(37);
         System.out.println(kugelpos);
-        textKugel.setText("Winning Number: " + String.valueOf(kugelpos) + "\n amount of spins" + String.valueOf(spin));
+        textKugel.setText("Winning Number: " + String.valueOf(kugelpos) + "\n amount of spins" + String.valueOf(spin)+"\n won:"+getwin());
+        resetbets();
+        System.out.println("--------------------------------------------");
     }
 
 
@@ -112,175 +67,232 @@ public class RoulettefeldController implements Initializable {
 
 
  public void spin(){
-       mediaPlayer.play();
+        mediaPlayer.seek(Duration.millis(0)); //jump to sec 0
+        mediaPlayer.play();
        //spindisplay.setText("ASD");
       }
 
     //Buttons
     public void zero(MouseEvent mouseEvent) {
         System.out.println("ZERO");
+        bets[0]=bets[0]+chips;
     }
 
     public void three(MouseEvent mouseEvent) {
         System.out.println("3");
+        bets[3]=bets[3]+chips;
     }
 
     public void two(MouseEvent mouseEvent) {
         System.out.println("2");
+        bets[2]=bets[2]+chips;
     }
 
     public void one(MouseEvent mouseEvent) {
         System.out.println("1");
+        bets[1]=bets[1]+chips;
     }
 
     public void six(MouseEvent mouseEvent) {
         System.out.println("6");
+        bets[6]=bets[6]+chips;
     }
 
     public void five(MouseEvent mouseEvent) {
         System.out.println("5");
+        bets[5]=bets[5]+chips;
     }
 
     public void four(MouseEvent mouseEvent) {
         System.out.println("4");
+        bets[4]=bets[4]+chips;
     }
 
     public void nine(MouseEvent mouseEvent) {
         System.out.println("9");
+        bets[9]=bets[9]+chips;
     }
 
     public void eight(MouseEvent mouseEvent) {
         System.out.println("8");
+        bets[8]=bets[8]+chips;
     }
 
     public void seven(MouseEvent mouseEvent) {
         System.out.println("7");
+        bets[7]=bets[7]+chips;
     }
 
     public void twelve(MouseEvent mouseEvent) {
         System.out.println("12");
+        bets[12]=bets[12]+chips;
     }
 
     public void eleven(MouseEvent mouseEvent) {
         System.out.println("11");
+        bets[11]=bets[11]+chips;
     }
 
     public void ten(MouseEvent mouseEvent) {
         System.out.println("10");
+        bets[10]=bets[10]+chips;
     }
 
     public void firsttwelve(MouseEvent mouseEvent) {
         System.out.println("1st12");
+        bets[37]=bets[37]+chips;
     }
 
     public void onetoeighteen(MouseEvent mouseEvent) {
         System.out.println("1to18");
+        bets[38]=bets[38]+chips;
     }
 
     public void even(MouseEvent mouseEvent) {
         System.out.println("even");
+        bets[39]=bets[39]+chips;
     }
 
     public void fifteen(MouseEvent mouseEvent) {
+        bets[15]=bets[15]+chips;
     }
 
     public void fourteen(MouseEvent mouseEvent) {
+        bets[14]=bets[14]+chips;
     }
 
     public void thirteen(MouseEvent mouseEvent) {
+        bets[13]=bets[13]+chips;
     }
 
     public void secoundtwelve(MouseEvent mouseEvent) {
+        bets[40]=bets[40]+chips;
     }
 
     public void thirdtwelve(MouseEvent mouseEvent) {
+        bets[41]=bets[41]+chips;
     }
 
     public void red(MouseEvent mouseEvent) {
-
+        bets[42]=bets[42]+chips;
     }
 
     public void black(MouseEvent mouseEvent) {
+        bets[43]=bets[43]+chips;
+       // System.out.println("DEBUG BLACK");
     }
 
     public void odd(MouseEvent mouseEvent) {
+        bets[44]=bets[44]+chips;
     }
 
     public void nineteentothirtysix(MouseEvent mouseEvent) {
+        bets[45]=bets[45]+chips;
     }
 
     public void eighteen(MouseEvent mouseEvent) {
+        bets[18]=bets[18]+chips;
     }
 
     public void seventeen(MouseEvent mouseEvent) {
+        bets[17]=bets[17]+chips;
     }
 
     public void sixteen(MouseEvent mouseEvent) {
+        bets[16]=bets[16]+chips;
     }
 
     public void twentyfour(MouseEvent mouseEvent) {
+        bets[24]=bets[24]+chips;
     }
 
     public void twentythree(MouseEvent mouseEvent) {
+        bets[23]=bets[23]+chips;
     }
 
     public void twentytwo(MouseEvent mouseEvent) {
+        bets[22]=bets[22]+chips;
     }
 
     public void twentyseven(MouseEvent mouseEvent) {
+        bets[27]=bets[27]+chips;
     }
 
     public void twentysix(MouseEvent mouseEvent) {
+        bets[26]=bets[26]+chips;
     }
 
     public void twentyfive(MouseEvent mouseEvent) {
+        bets[25]=bets[25]+chips;
     }
 
     public void thirty(MouseEvent mouseEvent) {
+        bets[30]=bets[30]+chips;
     }
 
     public void twentynine(MouseEvent mouseEvent) {
+        bets[19]=bets[19]+chips;
     }
 
     public void twetnyeight(MouseEvent mouseEvent) {
+        bets[28]=bets[28]+chips;
     }
 
     public void thirtythree(MouseEvent mouseEvent) {
+        bets[33]=bets[33]+chips;
     }
 
     public void thirtytwo(MouseEvent mouseEvent) {
+        bets[32]=bets[32]+chips;
     }
 
     public void thirtyone(MouseEvent mouseEvent) {
+        bets[31]=bets[31]+chips;
     }
 
     public void thirtysix(MouseEvent mouseEvent) {
+        bets[36]=bets[36]+chips;
     }
 
     public void thirtyfive(MouseEvent mouseEvent) {
+        bets[35]=bets[35]+chips;
     }
 
     public void thirtyfour(MouseEvent mouseEvent) {
+        bets[34]=bets[34]+chips;
     }
 
     public void row2(MouseEvent mouseEvent) {
+        bets[47]=bets[47]+chips;
     }
 
     public void row3(MouseEvent mouseEvent) {
+        bets[48]=bets[48]+chips;
     }
 
     public void row1(MouseEvent mouseEvent) {
+        bets[49]=bets[49]+chips;
     }
 
     public void twentyone(MouseEvent mouseEvent) {
+        bets[21]=bets[21]+chips;
     }
 
     public void twenty(MouseEvent mouseEvent) {
+        bets[20]=bets[20]+chips;
     }
 
     public void nineteen(MouseEvent mouseEvent) {
+        bets[19]=bets[19]+chips;
     }
 
-    public void calculateWin () {
+    public int getspin() {
+        return calc.spinit();
+    }
+    public int getwin() { return calc.calculateWin(bets); }
+    private void resetbets() {
+        for (int i = 0; i <50 ; i++) {
+          bets[i]=0;
+        }
     }
 }
